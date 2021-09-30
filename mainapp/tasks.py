fb6c6381b83599bec48b62c9c106c52eaf4bcf2e
harshkanani014 <harshkanani100@gmail.com>
2021-09-30 20:20:07 +0530

    celery conf

---
+++ mainapp/tasks.py
@@ -0,0 +1,52 @@
+import asyncio
+from celery import shared_task
+import time
+from yahoo_fin.stock_info import * 
+from threading import Thread
+import queue
+from channels.layers import get_channel_layer
+import simplejson as json
+
+@shared_task(bind=True)
+def update_stock(self, stockpicker):
+    data = {}
+    available_stocks = tickers_nifty50()
+    for i in stockpicker:
+        if i in available_stocks:
+           pass
+        else:
+            stockpicker.remove(i)
+    n_threads = len(stockpicker)
+    thread_list = []
+    que = queue.Queue()
+    start = time.time()
+    # for i in stockpicker:
+    #     result = get_quote_table(i)
+    #     data.update({i:result})
+
+    for i in range(n_threads):
+        thread = Thread(target = lambda q, arg1: q.put({stockpicker[i]: json.loads(json.dumps(get_quote_table(arg1), ignore_nan = True))}), args = (que, stockpicker[i]))
+        thread_list.append(thread)
+        thread_list[i].start()
+
+    for thread in thread_list:
+        thread.join()
+    
+    while not que.empty():
+        result = que.get()
+        data.update(result)
+    end = time.time()
+    time_taken = end - start
+    #print(time_taken)
+
+    # send data to group
+
+    channel_layer = get_channel_layer()
+    loop = asyncio.new_event_loop()
+
+    asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(channel_layer.group_send("stock_track", {
+        'type': 'send_stock_update',
+        'message': data,
+    }))
