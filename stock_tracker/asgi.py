582898c38c5133dd7c276b3a450b4b8973f4c987
harshkanani014 <harshkanani100@gmail.com>
2021-09-30 20:20:42 +0530

    set up asgi server

--- stock_tracker/asgi.py
+++ stock_tracker/asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+from channels.auth import AuthMiddlewareStack
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from mainapp.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_tracker.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket" : AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
